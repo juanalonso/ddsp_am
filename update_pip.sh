106d9a6def9144b4b036023e468d6a023e2dc733
Juan Alonso <juan@neuronasmuertas.com>
2021-03-04 13:00:00 +0100

    Create .gitignore. Update package generation

--- update_pip.sh
+++ update_pip.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -ex
 
-orig_dir=$(pwd)
-tmp_dir=$(mktemp -d -t ddsp-XXXX)
-git clone https://github.com/magenta/ddsp.git $tmp_dir
-cd $tmp_dir
+#orig_dir=$(pwd)
+#tmp_dir=$(mktemp -d -t ddsp-XXXX)
+#git clone https://github.com/magenta/ddsp.git $tmp_dir
+#cd $tmp_dir
 
 python setup.py sdist
 python setup.py bdist_wheel --universal
-twine upload dist/*
 
-cd $orig_dir
-rm -rf $tmp_dir
+rm -rf build
+rm -rf ddsp.egg-info
+rm -rf .eggs
+rm -rf ddsp/__pycache__
+
+cp dist/ddsp* /Volumes/GoogleDrive/Mi\ unidad/SMC\ 09/DDSP/dist
+
+#cd $orig_dir
+#rm -rf $tmp_dir
